d75b6e6688800ba79fca33d25de6752f77a21232
Leo Garza <leog4za@gmail.com>
2025-06-03 21:02:11 -0500

    updating images

--- src/components/sections/About3.jsx
+++ src/components/sections/About3.jsx
@@ -12,15 +12,15 @@ const About3Section = () => {
         <p dangerouslySetInnerHTML={{ __html: Data.description }} />
       </div>
       <div className="container">
-        <div className="row">
+        <div className="row" style={{ display: 'flex', flexWrap: 'wrap', rowGap: '1rem' }}>
           {Data.items.map((item, key) => (
             <div
               key={`about3-item-${key}`}
-              className="col-lg-4 col-md-6 col-sm-12"
+              className="col-lg-4 col-md-6 col-sm-12" style={{ gap: '1rem' }} 
             >
-              <div className="history-data">
-                <figure>
-                  <img className="w-100" src={item.image} alt={item.title} />
+              <div className="history-data" style={{ height: '100%' }}>
+                <figure style={{ height: '100%' }}>
+                  <img className="w-100" src={item.image} alt={item.title} style={{ height: '100%' }} />
                 </figure>
                 <div className="details">
                   <h3>{item.title}</h3>
